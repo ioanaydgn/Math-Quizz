64b883a6105c1060d2e5e6fe88e455f88d4c9b28
Mert Aydogan <mertaydogn0@gmail.com>
2023-05-13 00:59:19 +0300

    Initial Commit

---
+++ MathQuizz/MathQuizzApp.swift
@@ -0,0 +1,17 @@
+//
+//  MathQuizzApp.swift
+//  MathQuizz
+//
+//  Created by Mert Aydoğan on 13.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct MathQuizzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

f3d111ef0e29a52173bb99a05e6e43b73449bd5d
Mert Aydogan <mertaydogn0@gmail.com>
2023-05-13 10:23:54 +0300

    .

--- MathQuizz/ContentView.swift
+++ MathQuizz/ContentView.swift
@@ -1,28 +1,14 @@
-//
-//  ContentView.swift
-//  MathQuizz
-//
-//  Created by Mert Aydoğan on 13.05.2023.
-//
-
 import SwiftUI
 
 struct Question {
     var text: String
-    var answer: String
+    var options: [String]
+    var correctOptionIndex: Int
 }
 
 struct ContentView: View {
-    
-    /*@State private var questions = [
-        Question(text: "2 + 2 =", answer: "4"),
-        Question(text: "5 x 3 =", answer: "15"),
-        Question(text: "10 - 7 =", answer: "3")
-    ]*/
-    
     @State private var currentQuestion: Question?
     @State private var score = 0
-    @State private var userAnswer = ""
     @State private var showAlert = false
     
     var body: some View {
@@ -31,85 +17,78 @@ struct ContentView: View {
             Text(currentQuestion?.text ?? "")
                 .font(.largeTitle)
                 .padding()
-            
-            TextField("Enter your answer", text: $userAnswer)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
                 .padding()
-            
-            Button(action: {
-                checkAnswer()
-            }, label: { Text("Reply")
-                    .font(.title)
-                    .padding()
-                    .background(Color.blue)
-                    .foregroundColor(.white)
-                    .cornerRadius(10)
-            })
-            .padding()
+            if let options = currentQuestion?.options {
+                ForEach(0..<options.count) { index in
+                    Button(action: {
+                        checkAnswer(selectedOptionIndex: index)
+                    }) {
+                        Text(options[index])
+                            .font(.title)
+                            .padding()
+                            .frame(width: 200)
+                            .background(Color.blue)
+                            .foregroundColor(.white)
+                            .cornerRadius(10)
+                        
+                    }
+                    .padding(-1)
+                }
+            }
         }
         .alert(isPresented: $showAlert, content: {
             Alert(
                 title: Text("Game Over"),
-                message: Text("Scores: \(score)"),
-                dismissButton: .default(Text("Play again"), action: {
+                message: Text("Your score: \(score)"),
+                dismissButton: .default(Text("Play Again"), action: {
                     restartGame()
                 })
             )
         })
+        .onAppear {
+            generateQuestion()
+        }
     }
-    /*
-    func checkAnswer() {
-        let userEnteredAnswer = userAnswer.trimmingCharacters(in: .whitespacesAndNewlines)
-        let correctAnswer = questions[currentQuestion].answer
+    
+    func checkAnswer(selectedOptionIndex: Int) {
+        guard let question = currentQuestion else { return }
         
-        if userEnteredAnswer == correctAnswer {
+        if selectedOptionIndex == question.correctOptionIndex {
             score += 1
         } else {
             showAlert = true
         }
         
-        if currentQuestion < questions.count - 1 {
-            currentQuestion += 1
-            userAnswer = ""
-        } else {
-            showAlert = true
-        }
+        generateQuestion()
     }
-     */
-    func checkAnswer() {
-            let userEnteredAnswer = userAnswer.trimmingCharacters(in: .whitespacesAndNewlines)
-            let correctAnswer = currentQuestion?.answer ?? ""
-            
-            if userEnteredAnswer == correctAnswer {
-                score += 1
+    
+    func generateQuestion() {
+        
+        let correctOptionIndex = Int.random(in: 0..<4)
+        let correctAnswer = String(Int.random(in: 1...10))
+        
+        var options = [String]()
+        for i in 0..<4 {
+            if i == correctOptionIndex {
+                options.append(correctAnswer)
             } else {
-                showAlert = true
+                // Yanlış cevapları rastgele seçebilirsiniz
+                let wrongAnswer = String(Int.random(in: 1...10))
+                options.append(wrongAnswer)
             }
-            
-            generateQuestion()
-            userAnswer = ""
         }
         
-        func generateQuestion() {
-            // Soru oluşturulurken buradaki mantığı değiştirebilirsiniz
-            // Örnek olarak, rastgele iki sayı alıp toplama işlemi yapabilirsiniz
-            let randomNumber1 = Int.random(in: 1...10)
-            let randomNumber2 = Int.random(in: 1...10)
-            let questionText = "\(randomNumber1) + \(randomNumber2) ="
-            let correctAnswer = String(randomNumber1 + randomNumber2)
-            
-            currentQuestion = Question(text: questionText, answer: correctAnswer)
-        }
+        let questionText = "Which number is correct?"
         
+        currentQuestion = Question(text: questionText, options: options, correctOptionIndex: correctOptionIndex)
+    }
     
     func restartGame() {
         currentQuestion = nil
         score = 0
-        userAnswer = ""
         showAlert = false
-
+        
         generateQuestion()
-        //questions.shuffle()
     }
 }
 
